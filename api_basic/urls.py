99fffc8a439d86d05fa2ce8b6faecaf2ee5703d6
JeZoS <prabhat262111@gmail.com>
2021-07-01 10:58:34 +0530

    viewsets

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import ArticleAPIView, ArticleDetails, article_list,article_detail
+from django.urls.conf import include
+from rest_framework import routers
+from .views import ArticleAPIView,ArticleViewSet ,ArticleDetails, GenericAPIView, article_list,article_detail
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article',ArticleViewSet,basename='article')
 
 urlpatterns = [
+    path('viewset/',include(router.urls)),
     # path('article',article_list),
     path('article',ArticleAPIView.as_view()),
     # path('detail/<int:pk>',article_detail)
-    path('detail/<int:id>',ArticleDetails.as_view())
-
+    path('detail/<int:id>',ArticleDetails.as_view()),
+    path('generic/article/<int:id>',GenericAPIView.as_view())
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from django.http import HttpResponse, JsonResponse
 from rest_framework import serializers
+from rest_framework import permissions
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
@@ -9,14 +10,76 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self,request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    def create(self,request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset,pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    def update(self,request,pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+# generic classes
+class GenericAPIView(generics.GenericAPIView, mixins.DestroyModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    # authentication_classes = [SessionAuthentication,BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
+
+
+# classes
 class ArticleAPIView(APIView):
-    def get(self,request):
+    def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    def post(self,request):
+    def post(self, request):
         # data = JSONParser().parse(request)
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,33 +89,34 @@ class ArticleAPIView(APIView):
 
 
 class ArticleDetails(APIView):
-    def get_object(self,id):
+    def get_object(self, id):
         try:
             return Article.objects.get(pk=id)
         except Article.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self,request,id):
+    def get(self, request, id):
         article = self.get_object(id)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
 
-    def put(self,request,id):
+    def put(self, request, id):
         article = self.get_object(id)
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self,request,id):
+    def delete(self, request, id):
         article = self.get_object(id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# functions
 # @csrf_exempt
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
@@ -68,7 +132,8 @@ def article_list(request):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 # @csrf_exempt
 def article_detail(request, pk):
     try:
@@ -85,9 +150,9 @@ def article_detail(request, pk):
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   
+
     elif request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
